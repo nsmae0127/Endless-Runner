aa0a13cbeee78bebed7d827d0d75838b0cf587c7
nsmae0127 <nsmae0127@gmail.com>
2015-08-06 20:53:40 +0900

    Updated150806

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+    public Vector3 dir;
+
+    // Use this for initialization
+    void Start()
+    {
+        dir = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (dir == Vector3.forward)
+            {
+                dir = Vector3.left;
+            } else
+            {
+                dir = Vector3.forward;
+            }
+        }
+
+        float amountToMove = speed * Time.deltaTime;
+
+        transform.Translate(dir * amountToMove);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
