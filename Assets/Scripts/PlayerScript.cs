9363616a2b0eb43a0afb8849ba6ffba0153f8d6e
nsmae0127 <nsmae0127@gmail.com>
2015-08-11 18:33:52 +0900

    Updated150811

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,30 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
+    // The player's movement speed
     public float speed;
 
+    // The direction the player is traveling ing
     public Vector3 dir;
 
+    // A reference to the player's particle system
     public GameObject ps;
 
+    // Indicates if the player is dead
     private bool isDead;
 
+    // A reference to the reset button
     public GameObject resetBtn;
+
+    private int score = 0;
+
+    public Text scoreText;
 
     // Use this for initialization
     void Start()
     {
+        // Makes sure that the player is alive when the game starts
         isDead = false;
+
+        // Sets the player's direction to 0, so that the player doesn't move when the game start
         dir = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // If we click on the screen or the first mouse button
         if (Input.GetMouseButtonDown(0) && !isDead)
         {
+            score++;
+            scoreText.text = score.ToString();
+
+            // Switches the players direction every time we click on the screen or mouse
             if (dir == Vector3.forward)
             {
                 dir = Vector3.left;
@@ -35,20 +53,26 @@ public class PlayerScript : MonoBehaviour
             }
         }
 
+        // Calculates the player's movement
         float amountToMove = speed * Time.deltaTime;
 
+        // Makes the player move
         transform.Translate(dir * amountToMove);
     }
 
+    // When the player enters a trigger
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Pickup")
+        if (other.tag == "Pickup") // If its a pick up able objects
         {
             other.gameObject.SetActive(false);
             Instantiate(ps, transform.position, Quaternion.identity);
+            score += 3;
+            scoreText.text = score.ToString();
         }
     }
 
+    // When the player exits a trigger
     void OnTriggerExit(Collider other)
     {
         if (other.tag == "Tile")
